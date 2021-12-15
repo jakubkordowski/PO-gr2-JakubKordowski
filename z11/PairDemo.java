d31878d4c75424dbedc4751520056702ca0effc1
jakubkordowski <92037279+jakubkordowski@users.noreply.github.com>
2021-12-15 16:12:26 +0100

    Add files via upload
    Added z11, 11.6 need finish

---
+++ z11/PairDemo.java
@@ -0,0 +1,4 @@
+package pl.imiajd.kordowski;
+
+public class PairDemo {
+}
---
+++ z11/PairUtil.java
@@ -0,0 +1,7 @@
+package pl.imiajd.kordowski;
+
+public class PairUtil {
+    public static Pair swap(Pair p){
+        return new Pair(p.getFirst(), p.getSecond());
+    }
+}
